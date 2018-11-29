c7b684cdd287340d2a3588860645a011cc6f4d50
trumat <matru@protonmail.com>
2018-11-29 20:19:11 +0000

    Initial commit

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-  var apiRoot = 'http://localhost:8080/v1/task/';
+  var apiRoot = 'https://polar-plateau-30682.herokuapp.com/v1/task/';
   var datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   var tasksContainer = $('[data-tasks-container]');
 
