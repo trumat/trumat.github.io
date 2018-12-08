cd3d1c7382fbba75ad6599dbdbed05d3e00066fa
trumat <matru@protonmail.com>
2018-12-08 15:20:11 +0000

     Exercise 19.1

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
-  const apiRoot = 'http://localhost:8080/v1/task/';
-  const trelloApiRoot = 'http://localhost:8080/v1/trello/';
+  const apiRoot = 'https://polar-plateau-30682.herokuapp.com/v1/task/';
+  const trelloApiRoot = 'https://polar-plateau-30682.herokuapp.com/v1/trello/';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
 
